559988b7cdfab9e6088a11d6ea22ae0b8fc8f7af
Elkin Lopez <40122667+elkinzepol@users.noreply.github.com>
2022-08-25 02:24:15 -0500

    inserta Mensaje aquí

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# portfolio
- Elkin
+#  Proyecto
+##  Portfolio Elkin López
+
+## Comenzando 🚀
+_Creacion de Portafolio Personal_
+
+
+![Portfolio](pendiente link)
+
+### Herramientas usadas en el proyecto 🛠️
+
+* [Boostrap 4](httpsgetbootstrap.com) El framework web
+* [Animate.css ](httpsdaneden.github.ioanimate.css) Libreria CSS
+* [WOW.css ](https://wowjs.uk/) Libreria CSS
+* [Font Awesome](httpsfontawesome.com) El framework de iconos vectoriales
+* [Stylus](http://stylus-lang.com/) Preprocesador CSS
---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+const navbar = document.querySelector(".navbar");
+const homebg = document.querySelector(".home__background");
+const menu = document.querySelector(".navbar__menu");
+const links = document.querySelector(".navbar__links");
+const sections = document.querySelectorAll("section");
+const config = {
+  rootMargin: "0px",
+  threshold: [0.6, 0.9],
+};
+
+function handleLlinks() {
+  if (window.innerWidth <= 991) {
+    links.classList.toggle("visible");
+  }
+}
+
+menu.addEventListener("click", handleLlinks);
+links.addEventListener("click", handleLlinks);
+
+window.addEventListener("scroll", function () {
+  window.scrollY > 100 && (navbar.style.background = `rgba(0,0,0,0.9)`);
+  window.scrollY < 100 && (navbar.style.background = `transparent`);
+});
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      intersectionHandler(entry);
+    }
+  });
+}, config);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+function intersectionHandler(entry) {
+  const id = entry.target.id;
+  const currentlyActive = document.querySelector(".navbar__links  .active");
+  const shouldBeActive = document.querySelector(
+    ".navbar__links [data-ref=" + id + "]"
+  );
+
+  if (currentlyActive) {
+    currentlyActive.classList.remove("active");
+  }
+  if (shouldBeActive) {
+    shouldBeActive.classList.add("active");
+  }
+}
+
+const typed = new Typed("#typed", {
+  strings: ["- Websites -", "- Branding -", "- Frontend Developer -"],
+  // backSpeed: 40,
+  // typeSpeed: 40
+  backDelay: 2000,
+  typeSpeed: 45,
+  loop: true,
+});
